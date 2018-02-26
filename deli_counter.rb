4146921f78e65ce49f31613602fd11dd47f02fac
Rani Shah <rani76@gmail.com>
2018-02-26 23:19:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,19 +1,21 @@
 katz_deli = []
 
-def line(katz_deli) 
-if katz_deli.size > 0 
- katz_deli.all? { |linenumber| 
-  "You are number" + linenumber+1 + "in the line" }
-else 
-  return "This line is empty!"
-end 
+def line(katz_deli)
+  if katz_deli.empty?
+    puts "The line is currently empty."
+  else
+    current_line = "The line is currently:"
+    katz_deli.each.with_index(1) do |person, i|
+      current_line << " #{i}. #{person}"
+    end
+    puts current_line
+  end
 end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
   return name + ", you are number " + katz_deli.size-1 + " in the line!"
-end 
+end
 end
 
-def now_serving(katz_deli) 
-  
+def now_serving(katz_deli)
