b97ec64ad2458f59015ad775e6b6f32c17c287c4
Rani Shah <rani76@gmail.com>
2018-02-26 23:18:04 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+katz_deli = []
+
+def line(katz_deli) 
+if katz_deli.size > 0 
+ katz_deli.all? { |linenumber| 
+  "You are number" + linenumber+1 + "in the line" }
+else 
+  return "This line is empty!"
+end 
+end
+
+def take_a_number(katz_deli, name)
+  katz_deli.push(name)
+  return name + ", you are number " + katz_deli.size-1 + " in the line!"
+end 
+end
+
+def now_serving(katz_deli) 
+  
