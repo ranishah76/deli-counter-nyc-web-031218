869f8650b08db9572e5cbff480fb886f4d368724
Rani Shah <rani76@gmail.com>
2018-02-26 23:22:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,8 +14,14 @@ end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  return name + ", you are number " + katz_deli.size-1 + " in the line!"
-end
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving(katz_deli)
+ if katz_deli.empty?
+   puts "The line is empty"
+ else 
+   puts "Currently serving #{katz_deli.first}"
+   katz_deli.shift
+ end
+ end
